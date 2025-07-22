5ebb88a1a02d8182b31892c77cfa6128cdaf1e16
Arti_Jadhav <arti.jadhav@wynum.com>
2025-07-22 16:43:55 +0530

    added comments

--- main_test.go
+++ main_test.go
@@ -6,6 +6,7 @@ import "testing"
 func TestAdd(t *testing.T) {
 	result := Add(2, 6)
 	expected := 8
+	//added comments
 
 	if result != expected {
 		t.Errorf("Expected %d, but got %d", expected, result)
